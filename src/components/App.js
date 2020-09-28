5e9d453baee22f7a7b5191faa7ba5157d7bd85a6
User <user@Users-MacBook-Pro.local>
2020-09-29 01:07:31 +0300

    added: index.js, App.js, App.css

---
+++ src/components/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import '../css/App.css';
+
+const App = () => {
+    return (<div className="App">
+        <h1>App component check</h1>
+    </div>)
+}
+
+export default App;
