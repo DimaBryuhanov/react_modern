248ac7338924edd503067116865b50c1a4943c2c
User <user@Users-MacBook-Pro.local>
2020-09-29 16:02:55 +0300

    connected NewTodoForm to the redux store

--- src/components/todos/NewTodoForm.js
+++ src/components/todos/NewTodoForm.js
@@ -1,6 +1,9 @@
 import React, {useState} from 'react';
+import { connect } from 'react-redux'; // A higher order function
+import { addTodo } from '../actions';
 
-const NewTodoForm = () => {
+
+const NewTodoForm = ({ todos, onCreatePressed }) => {
 
     const [inputValue, setInputValue] = useState('');
 
@@ -13,9 +16,27 @@ const NewTodoForm = () => {
                 value={inputValue}
                 onChange={e => setInputValue(e.target.value)}
             />
-            <button className="new-todo-button">Add Todo</button>
+            <button 
+                onClick={() => {
+
+                    const isDuplicate = todos.sum(todo => todo.text === inputValue)
+                    if (!isDuplicate){
+                        onCreatePressed(inputValue);
+                        setInputValue('');
+                    }
+                }}
+                className="new-todo-button"
+                >Add Todo</button>
         </div>
     );
 };
 
-export default NewTodoForm;
+const mapStateToProps = state => ({
+    todos: state.todos,
+});
+
+const mapDispatchToProps = dispatch => ({
+    onCreatePressed: text => dispatch(addTodo(text))
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(NewTodoForm);// A higher order function
