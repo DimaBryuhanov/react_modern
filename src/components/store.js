f3d445735e65b8f6d9af896bb14c1a920ec60534
User <user@Users-MacBook-Pro.local>
2020-10-15 20:48:37 +0300

    persisting the Redux Store

--- src/components/store.js
+++ src/components/store.js
@@ -1,4 +1,7 @@
 import { createStore, combineReducers } from 'redux';
+import { persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+import autoMergeLevel2 from 'redux-persist/lib/stateReconciler/autoMergeLevel2';
 import { todos } from './reducers';
 
 //Hooking to redux store:
@@ -6,6 +9,14 @@ const reducers = {
     todos,
 };
 
-const rootReducer = combineReducers(reducers);
+const persistConfig = {
+    key: 'root',
+    storage,
+    stateReconciler: autoMergeLevel2 //Tells reduxPersist how to reconcile initial & stored states of the application
+};
 
-export const configureStore = () => createStore(rootReducer);
+const rootReducer = combineReducers(reducers);
+//Tells the reducer 1. How to save & 2. where to store the data:
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+export const configureStore = () => createStore(persistedReducer);
--- src/index.js
+++ src/index.js
@@ -1,14 +1,23 @@
 //Libraries' components:
 import React from 'react';
 import { Provider } from 'react-redux';
+import { persistStore } from 'redux-persist';
+import { PersistGate } from 'redux-persist/lib/integration/react';
 import ReactDOM from 'react-dom';
 // Local components:
 import App from './components/App';
 import { configureStore} from './components/store';
 
+const store = configureStore();
+const persistor = persistStore(store); //Saves the store in the local storage
+
 ReactDOM.render(
-    <Provider store={configureStore()}>
-        <App />
+    <Provider store={store}>
+        <PersistGate 
+            loading={<div>Loading...</div>} //The view displayed while the App data is being loaded
+            persistor={persistor}>
+            <App />
+        </PersistGate>
     </Provider>,
     document.getElementById('root')
 );
