f634de2580799eea8c4f98c8fcc7d8b8dc19aa1e
Dmitry Brook <dmitrii_bryuhanov@mail.ru>
2020-10-22 18:50:07 +0300

    fixing error: addTodoRequest -> dispatch parameter

--- src/components/todos/thunks.js
+++ src/components/todos/thunks.js
@@ -1,10 +1,10 @@
-import { 
-    loadTodosInProgress, 
-    removeTodo, 
-    loadTodosSuccess, 
+import {
+    loadTodosInProgress,
+    removeTodo,
+    loadTodosSuccess,
     loadTodosFailure,
     completeTodo,
-    addTodo  
+    addTodo
 } from '../actions';
 
 /**
@@ -13,54 +13,54 @@ import {
  * @param {*} getState - gets the current state of the store
  */
 export const loadTodos = () => async (dispatch, getState) => {
-    try{
+    try {
         dispatch(loadTodosInProgress());
         const response = await fetch('http://localhost:8080/todos-delay');
         const todos = await response.json();
-    
+
         dispatch(loadTodosSuccess(todos)); //used to put all the todos in the redux store
-    }catch(e){
+    } catch (e) {
         dispatch(loadTodosFailure());
         dispatch(displayAlert(e));
     }
-    
+
 };
 
-export const addTodoRequest = text => async () => {
-    try{
+export const addTodoRequest = text => async dispatch => {
+    try {
         const body = JSON.stringify({ text });
-    const response = await fetch('http://localhost:8080/todos', {
-        headers:{
-            "Content-Type":'application/json'
-        },
-        method: 'POST',
-        body,
-    });
-    const todo = await response.json();
-    dispatch(addTodo(todo));
-    }catch(e){
+        const response = await fetch('http://localhost:8080/todos', {
+            headers: {
+                "Content-Type": 'application/json'
+            },
+            method: 'POST',
+            body,
+        });
+        const todo = await response.json();
+        dispatch(addTodo(todo));
+    } catch (e) {
         dispatch(displayAlert(e));
     }
 };
 
 export const removeTodoRequest = id => async dispatch => {
-    try{
+    try {
         const response = await fetch(`http://localhost:8080/todos/${id}`,
-        {method: 'DELETE'});
+            { method: 'DELETE' });
         const removedTodo = await response.json();
         dispatch(removeTodo(removedTodo));
-    }catch(e){
+    } catch (e) {
         dispatch(displayAlert(e));
     }
 }
 
 export const isCompletedRequest = id => async dispatch => {
-    try{
+    try {
         const response = await fetch(`http://localhost:8080/todos/${id}/completed`,
-        {method: 'post'});
+            { method: 'post' });
         const updatedTodo = await response.json();
         dispatch(completeTodo(updatedTodo));
-    }catch(e){
+    } catch (e) {
         dispatch(displayAlert(e));
     }
 };
