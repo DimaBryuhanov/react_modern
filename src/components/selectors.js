c97a6d1fe65e4787ad22c3e31f240df8f465a620
Dmitry Brook <dmitrii_bryuhanov@mail.ru>
2020-10-22 20:24:44 +0300

    Adding getIncompleteTodos selector

--- src/components/selectors.js
+++ src/components/selectors.js
@@ -1,9 +1,14 @@
 //1. Help the components to be independent of the exact structure of data in the Redux store
 //2. give us a place to put the logic necessary for transforming bare Redux data into more specific data that our components might need
 
+import { createSelector } from 'reselect';
+
 export const getTodos = state => state.todos.data;
 export const getTodosLoading = state => state.todos.isLoading;
 
 
-//export const getIncompleteTodos
+export const getIncompleteTodos = createSelector(
+    getTodos,
+    todos.filter(todo => !todo.isCompleted)
+)
 
