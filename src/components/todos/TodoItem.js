9b1fe87de315e4646cc776760536195546c7a1ae
Dmitry Brook <dmitrii_bryuhanov@mail.ru>
2020-10-23 05:55:03 +0300

    Adding: TodoItem styled components

--- src/components/todos/TodoItem.js
+++ src/components/todos/TodoItem.js
@@ -1,22 +1,53 @@
 import React from 'react';
+import styled from 'styled-components';
+
+//CSS Styled Components:
+const TodoItemContainer = styled.div`
+    background: #fff;
+    border-radius: 8px;
+    margin-top: 8px;
+    padding: 16px;
+    position: relative;
+    box-shadow: 0 4px 8px grey;
+`;
+
+const ButtonsContainer = styled.div`
+    position: absolute;
+    right: 12px;
+    bottom: 12px;
+`;
+
+const CompletedButton = styled.button`
+    display: inline-block;
+    background-color: #22ee22;
+`;
+
+const RemoveButton = styled.button`
+    display: inline-block;
+    background-color: #ee2222;
+    margin-left: 8px;
+`;
 
 const TodoItem = ({ todo, onRemovePressed, onCompletedPressed }) => {
     return (
-        <div className="todo-item-container">
+        <TodoItemContainer>
             <h3>{todo.text}</h3>
-            <div className="buttons-container">
-                {todo.isCompleted 
+            <ButtonsContainer>
+                {todo.isCompleted
                     ? null
-                    : <button
-                        className="completed-button"
+                    : <CompletedButton
                         onClick={() => onCompletedPressed(todo.id)}
-                    >Completed</button>}
-                <button
-                    className="remove-button"
-                    onClick={() => onRemovePressed(todo.id)}
-                >Remove</button>
-            </div>
-        </div>
+                    >
+                        Completed
+                    </CompletedButton>
+                }
+                <RemoveButton
+                onClick={() => onRemovePressed(todo.id)}
+                >
+                Remove
+                </RemoveButton>
+            </ButtonsContainer>
+        </TodoItemContainer>
     );
 };
 
